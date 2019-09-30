2ee654a39a79e30a2fda7cbfdaec59e887ddf4b2
Fletcher <odmoandtheboy@gmail.com>
2019-09-30 13:26:15 +0100

    Added Noise To Lamp Range

--- Assets/Scripts/Player/Lamp_Controller.cs
+++ Assets/Scripts/Player/Lamp_Controller.cs
@@ -11,7 +11,8 @@ public class Lamp_Controller : MonoBehaviour
 
     [SerializeField] bool m_mainLamp = true;
 
-    public Shader m_shader;
+    [SerializeField] private float m_noiseScale = 1.0f;
+    [SerializeField] private List<AnimationCurve> m_noiseCurves;
 
     private void Start()
     {
@@ -20,9 +21,15 @@ public class Lamp_Controller : MonoBehaviour
 
     private void Update()
     {
+        float newRange = m_range;
+        foreach(AnimationCurve curve in m_noiseCurves)
+        {
+            newRange += (curve.Evaluate(Time.time) * m_noiseScale);
+        }
+
         if(m_mainLamp)
         {
-            Shader.SetGlobalFloat("LampRange", m_range);
+            Shader.SetGlobalFloat("LampRange", newRange);
             Shader.SetGlobalVector("LampPosition", transform.position);
         }
     }
