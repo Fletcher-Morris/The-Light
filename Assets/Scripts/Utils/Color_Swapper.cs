dbb396798f729cc3a395d289197b77e9eb87ca08
Fletcher <odmoandtheboy@gmail.com>
2019-09-30 15:10:57 +0100

    Minor Changes

--- Assets/Scripts/Utils/Color_Swapper.cs
+++ Assets/Scripts/Utils/Color_Swapper.cs
@@ -13,12 +13,12 @@ public class Color_Swapper : MonoBehaviour
     {
         if (i == 0)
         {
-            GetComponent<MeshRenderer>().material.SetColor("_BaseColor", m_colorA);
+            GetComponent<MeshRenderer>().material.SetColor("_Color", m_colorA);
             i = 1;
         }
         else
         {
-            GetComponent<MeshRenderer>().material.SetColor("_BaseColor", m_colorB);
+            GetComponent<MeshRenderer>().material.SetColor("_Color", m_colorB);
             i = 0;
         }
     }
