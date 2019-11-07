f2b04d2be6ce3c52b8da7fae632daa60296eb473
assadasadsas <assadasadsas@outlook.com>
2019-11-07 05:25:48 +0000

    update0
    Add maincharacter
    Add watermill
    Adjust material
    Adjust trees

---
+++ Assets/Scripts/Falltree.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falltree : MonoBehaviour
+{
+    public float fspeed=10f;
+    Vector3 fallaxis = new Vector3();
+    float angle = 0f;
+    bool fallen = false;
+    float No_0_random(float rmin,float rmax)
+    {
+        float a=0;
+        while (a == 0)
+        {
+            a = Random.Range(rmin, rmax);
+        }
+        return a;
+    }
+  public void Fall()
+    {
+        fallaxis.x = No_0_random(-1f, 1f);
+        fallaxis.y = 0;
+        fallaxis.z = No_0_random(-1f, 1f);
+        fallaxis = fallaxis.normalized;
+        StartCoroutine(TreeFall());
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Darkcloud")&&!fallen)
+        {
+            fallen = true;
+            Fall();
+        }
+    }
+    IEnumerator TreeFall()
+    {
+        while (angle < 90)
+        {
+            float x = Time.deltaTime * fspeed;
+            this.transform.Rotate(fallaxis, x);
+            angle += x;
+            fspeed = fspeed * 1.02f;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+   public GameObject target;
+    Vector3 vt = new Vector3();
+    public float mspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vt = target.transform.position - this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += vt * mspeed * Time.deltaTime;
+    }
+}
