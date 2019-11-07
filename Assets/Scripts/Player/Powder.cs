e6c871e9b665c74b0aa4b88130c95b9bc69f9abf
Fletcher <odmoandtheboy@gmail.com>
2019-11-07 14:24:17 +0000

    Added Burn-Time To Powders

--- Assets/Scripts/Player/Powder.cs
+++ Assets/Scripts/Player/Powder.cs
@@ -11,16 +11,16 @@ public class Powder : ScriptableObject
 
     [Tooltip("The name of the powder, visible to the player.")]
     [SerializeField] private string m_powderName;
-    public string GetName() { return m_powderName; }
+    public string PowderName { get => m_powderName; }
 
     [Tooltip("The color associated with this powder, for UI etc.")]
     [SerializeField] private Color m_powderColor;
-    public Color GetColor() { return m_powderColor; }
+    public Color PowderColor { get => m_powderColor; }
 
     [Tooltip("The description of this powder, visible to the player.")]
     [Multiline]
     [SerializeField] private string m_description;
-    public string GetDescription() { return m_description; }
+    public string Description { get => m_description; }
 
 
     [Header("Recipes")]
@@ -33,9 +33,17 @@ public class Powder : ScriptableObject
 
     [Header("Effects")]
 
+    [Tooltip("The how long the lingering effect of this powder last for in seconds.")]
+    [SerializeField] private float m_burnTime = 5.0f;
+    public float BurnTime { get => m_burnTime; }
 
+    [Tooltip("The stunning power of this powder, higher values stun tougher monsters.")]
     [SerializeField] private float m_stunPower = 0.0f;
-    public float StunPower() { return m_stunPower; }
+    public float StunPower { get => m_stunPower; set => m_stunPower = value; }
+
+    [Tooltip("The fear intensity of this powder, higher values scare tougher monsers.")]
+    [SerializeField] private float m_fearIntensity = 0.0f;
+    public float FearIntensity { get => m_fearIntensity; }
 
 }
 
