e4dc0af03ebc6d8941e44af5480f39b871c2aa79
Jean Paul <warcraftion@gmail.com>
2019-11-19 17:25:52 +0000

    Stairway to Watermill Added + Minor Adjustments
    Path to watermill is clearer, also fixed spawn location for when you exit the village.

--- Assets/Quests/Quest07/Quest7.cs
+++ Assets/Quests/Quest07/Quest7.cs
@@ -14,7 +14,7 @@ public class Quest7 : Quest
     public override void OnAdd()
     {
         Instantiate(uis);
-        Player_Controller.Singleton().transform.position = new Vector3(252.5f, 26.8f, -181.6f);
+        Player_Controller.Singleton().transform.position = new Vector3(249.65f, 25.31f, -191.28f);
         Player_Controller.Singleton().CamYAxis().eulerAngles = new Vector3(0, 0, 0);
     }
     public override void Check()
