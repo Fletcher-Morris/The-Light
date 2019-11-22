769dfd41ee2d8419a45203d9e063766d5e80b0e1
Fletcher Morris <odmoandtheboy@gmail.com>
2019-11-22 19:33:21 +0000

    Added 'Override_Lamps' Editor Script
    Added an editor button which toggles the effects of lamps on materials on/off.

---
+++ Assets/Scripts/Editor/Override_Lamps.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if UNITY_EDITOR
+
+public class Override_Lamps : Editor
+{
+
+    static int m_override = 0;
+
+    [MenuItem("Tools/Toggle Lamp Override")]
+    public static void ConvertMaterials()
+    {
+        m_override = 1 - m_override;
+        Shader.SetGlobalInt("OverrideLamps", m_override);
+    }
+}
+
+#endif
