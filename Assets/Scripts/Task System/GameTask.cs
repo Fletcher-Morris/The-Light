f5db4e52ca5a46cb1a99e0ba0327260cd861f34e
Fletcher Morris <odmoandtheboy@gmail.com>
2019-11-29 17:15:43 +0000

    Started Work On New Quest System (Tasks)
    Task_Manager stores active and completed tasks. It also runs logic for those tasks.
    Control_Task allows other scripts to activate tasks, complete tasks, and modify requirements of tasks.
    GameTask is the base class for tasks (quests).
    TaskRequirement is the base of logic for tasks, stored as an integer, when less than one, is is true. When all requirements on a task are true, the task is completed.

---
+++ Assets/Scripts/Task System/GameTask.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[CreateAssetMenu(fileName = "New Task", menuName = "Task", order = 1)]
+[System.Serializable]
+public class GameTask : ScriptableObject
+{
+    [SerializeField] private string taskName;
+    public string TaskName { get => taskName; set => taskName = value; }
+    [SerializeField] private string taskDescription;
+    public string TaskDescription { get => taskDescription; set => taskDescription = value; }
+
+    [SerializeField] private List<TaskRequirement> requirements;
+    public List<TaskRequirement> Requirements { get => requirements; set => requirements = value; }
+
+    public UnityEvent OnTaskStarted;
+    public UnityEvent OnTaskCompleted;
+
+    public GameTask (GameTask _task)
+    {
+        this.TaskName = _task.TaskName;
+        this.TaskDescription = _task.TaskDescription;
+        this.Requirements = _task.Requirements;
+        this.OnTaskStarted = _task.OnTaskStarted;
+        this.OnTaskCompleted = _task.OnTaskCompleted;
+    }
+
+
+    public bool CheckRequirements()
+    {
+        foreach(TaskRequirement req in requirements)
+        {
+            if (req.RequirementData >= 1) return false;
+        }
+        return true;
+    }
+
+    public void SetRequirementValue(string _requirementName, int _value)
+    {
+        GetRequirement(_requirementName)?.SetValue(_value);
+    }
+    public void SetRequirementValue(string _requirementName, bool _value)
+    {
+        SetRequirementValue(_requirementName, 1 - _value.ToInt());
+    }
+    public void ReduceRequirementValue(string _requirementName)
+    {
+        ReduceRequirementValue(_requirementName, 1);
+    }
+    public void ReduceRequirementValue(string _requirementName, int _reduceValue)
+    {
+        GetRequirement(_requirementName)?.ReduceValue(_reduceValue);
+    }
+
+    TaskRequirement GetRequirement(string _requirementName)
+    {
+        foreach(TaskRequirement req in requirements)
+        {
+            if (req.RequirementName == _requirementName) return req;
+        }
+        Debug.LogWarning("Could Not Find Requirement '" + _requirementName + "' In GameTask '" + TaskName + "'!");
+        return null;
+    }
+
+}
+
+
+[System.Serializable]
+public class TaskRequirement
+{
+    public string RequirementName = "";
+    public int RequirementData = 1;
+
+    public void ReduceValue(int _value)
+    {
+        RequirementData -= _value;
+        Debug.Log("Reduced Value Of Task Requirement '" + RequirementName + "' By " + _value + "!");
+    }
+    public void SetValue(int _value)
+    {
+        RequirementData = _value;
+        Debug.Log("Set Value Of Task Requirement '" + RequirementName + "' To " + _value + "!");
+    }
+}
---
+++ Assets/Scripts/Task System/Task_Manager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Task_Manager : MonoBehaviour
+{
+    private static Task_Manager m_singleton;
+    public static Task_Manager Singleton() { return m_singleton; }
+
+    [SerializeField] private List<GameTask> m_activateOnStart;
+
+    [SerializeField] private List<GameTask> m_activeTasks;
+    [SerializeField] private List<GameTask> m_completedTasks;
+
+
+    private void Awake()
+    {
+        if (m_singleton == null) m_singleton = this;
+    }
+
+    private void Start()
+    {
+        foreach(GameTask task in m_activateOnStart)
+        {
+            ActivateTask(task);
+        }
+    }
+
+    public static void ActivateTask(GameTask _task)
+    {
+        RemoveTask(_task.name);
+        Singleton().m_activeTasks.Add(new GameTask(_task));
+        Debug.Log("Activated Task '" + _task.TaskName + "'!");
+        _task.OnTaskStarted.Invoke();
+        UpdateTasks();
+    }
+
+    public static void UpdateTasks()
+    {
+        foreach(GameTask task in Singleton().m_activeTasks)
+        {
+            if (task.CheckRequirements()) CompleteTask(task);
+        }
+    }
+
+    public static GameTask GetTask(string _taskName)
+    {
+        GameTask task;
+        task = GetActiveTask(_taskName);
+        if (task != null) return task;
+        task = GetCompletedTask(_taskName);
+        if (task != null) return task;
+        return null;
+    }
+    public static GameTask GetActiveTask(string _taskName)
+    {
+        foreach (GameTask task in Singleton().m_activeTasks)
+        {
+            if (task.TaskName == _taskName) return task;
+        }
+        return null;
+    }
+    public static GameTask GetCompletedTask(string _taskName)
+    {
+        foreach (GameTask task in Singleton().m_completedTasks)
+        {
+            if (task.TaskName == _taskName) return task;
+        }
+        return null;
+    }
+
+    public static void RemoveTask(string _taskName)
+    {
+        RemoveTaskFromActive(_taskName);
+        RemoveTaskFromCompleted(_taskName);
+        UpdateTasks();
+    }
+    public static void RemoveTaskFromActive(string _taskName)
+    {
+        foreach (GameTask task in Singleton().m_activeTasks)
+        {
+            if (task.TaskName == _taskName)
+            {
+                RemoveTaskFromActive(task);
+            }
+        }
+    }
+    private static void RemoveTaskFromActive(GameTask _task)
+    {
+        if (_task == null) return;
+        Singleton().m_activeTasks.Remove(_task);
+        Debug.Log("Removed Task '" + _task.TaskName + " From Active Tasks!");
+    }
+    public static void RemoveTaskFromCompleted(string _taskName)
+    {
+        foreach (GameTask task in Singleton().m_activeTasks)
+        {
+            if (task.TaskName == _taskName)
+            {
+                RemoveTaskFromCompleted(task);
+            }
+        }
+    }
+    private static void RemoveTaskFromCompleted(GameTask _task)
+    {
+        if (_task == null) return;
+        Singleton().m_completedTasks.Remove(_task);
+        Debug.Log("Removed Task '" + _task.TaskName + " From Completed Tasks!");
+    }
+
+    public static void CompleteTask(string _taskName)
+    {
+        GameTask task = GetActiveTask(_taskName);
+        if(task != null)
+        {
+            CompleteTask(task);
+        }
+    }
+
+    private static void CompleteTask(GameTask _task)
+    {
+        Debug.Log("Completed Task '" + _task.TaskName + "!");
+        Singleton().m_completedTasks.Add(_task);
+        RemoveTaskFromActive(_task);
+        Singleton().m_completedTasks.Add(_task);
+        _task.OnTaskCompleted.Invoke();
+    }
+
+    public static bool TaskIsActive(string _taskName)
+    {
+        return Singleton().m_activeTasks.Contains(GetTask(_taskName));
+    }
+    public static bool TaskIsCompleted(string _taskName)
+    {
+        return Singleton().m_completedTasks.Contains(GetTask(_taskName));
+    }
+    public static bool TaskExists(string _taskName)
+    {
+        return GetTask(_taskName) != null;
+    }
+
+    public static void ModifyTaskRequirement(string _taskName, string _requirement, int _value, bool _reduce)
+    {
+        GameTask task = GetTask(_taskName);
+        if(task != null)
+        {
+            ModifyTaskRequirement(task, _requirement, _value, _reduce);
+        }
+    }
+    private static void ModifyTaskRequirement(GameTask _task, string _requirement, int _value, bool _reduce)
+    {
+        if(_reduce)
+        {
+            _task.ReduceRequirementValue(_requirement, _value);
+        }
+        else
+        {
+            _task.SetRequirementValue(_requirement, _value);
+        }
+
+        UpdateTasks();
+    }
+}
