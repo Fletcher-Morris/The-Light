f580b455ac9a7d50ba7f1da28ba1665f24cfb0b4
assadasadsas <assadasadsas@outlook.com>
2019-12-03 16:40:57 +0000

    Updates

--- Assets/Scripts/escapevillage.cs
+++ Assets/Scripts/escapevillage.cs
@@ -18,7 +18,7 @@ public class escapevillage : MonoBehaviour
             if (other.CompareTag("Player"))
             {
                
-                    triggered = true;
+                    //triggered = true;
                 Player_Controller.Singleton().transform.position = new Vector3(249.65f, 25.31f, -191.28f);
                 Player_Controller.Singleton().CamYAxis().eulerAngles = new Vector3(0, 0, 0);
             }
