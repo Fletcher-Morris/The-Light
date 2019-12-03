fab22dbd260b8e869c5a39908bcf5f2636fb4520
assadasadsas <assadasadsas@outlook.com>
2019-12-03 14:09:56 +0000

    removed questsystem fixed vfx

---
+++ Assets/Scripts/TriggerDarkness.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class TriggerDarkness : MonoBehaviour
+{
+    bool triggered=false;
+    public PlayableDirector director;
+    public List<GameObject> Darkclouds = new List<GameObject>();
+    public GameObject CM, CC;
+    public GameObject Beaconlight;
+   
+
+    [SerializeField] private Environment_Transition m_environmentTransition;
+    [SerializeField] private EnvironmentSettings m_darkEnvironment;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered)
+        {
+            
+            if (other.CompareTag("Player"))
+            {
+              
+                    triggered = true;
+                
+                
+                    Darkcome();
+              
+            }
+        }
+    }
+    void Darkcome()
+    {
+        Player_Controller.Singleton().InCutscene = true;
+        CM.SetActive(false);
+        CC.SetActive(true);
+        m_environmentTransition.Transition(m_darkEnvironment, 5.0f);
+        director.Play();
+    }
+    public void Turnoffbeacon()
+    {
+        Beaconlight.SetActive(false);
+    }
+   public void BackToGame()
+    {
+        CC.SetActive(false);
+        CM.SetActive(true);
+        Player_Controller.Singleton().InCutscene = false;
+        
+    }
+    public void ActiveDarkclouds()
+    {
+        foreach(GameObject g in Darkclouds)
+        {
+            g.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/escapevillage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escapevillage : MonoBehaviour
+{
+    bool triggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered)
+        {
+
+            if (other.CompareTag("Player"))
+            {
+               
+                    triggered = true;
+                Player_Controller.Singleton().transform.position = new Vector3(249.65f, 25.31f, -191.28f);
+                Player_Controller.Singleton().CamYAxis().eulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
