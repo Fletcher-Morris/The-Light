60682e487fd29ecdb270e181ef917ac7d4f9e0af
sara <sara_quenel@hotmail.fr>
2019-12-10 11:29:50 +0000

    Fix the druid camp animation

---
+++ Assets/Models/DruidCamp/BagFollowHands.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagFollowHands : MonoBehaviour
+{
+
+    public Transform Follower, Followed;
+    public Vector3 offset;
+
+    private void OnEnable()
+    {
+        Follower.parent = Followed;
+    }
+    // Update is called once per frame
+    /*void Update()
+    {
+        Follower.transform.position = Followed.transform.position + offset;
+        Follower.transform.rotation = Followed.transform.rotation;
+    }*/
+}
