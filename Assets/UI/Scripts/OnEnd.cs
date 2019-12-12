93dcf3bf77bf3aa45bb8cb9b622fc776d7e691b4
sara <sara_quenel@hotmail.fr>
2019-12-12 10:23:29 +0000

    Add the end

---
+++ Assets/UI/Scripts/OnEnd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnEnd : MonoBehaviour
+{
+
+    public GameObject UIEnd;
+
+    private void Start()
+    {
+        UIEnd.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player_Controller>().enabled = false;
+
+            UIEnd.SetActive(true);
+            StartCoroutine(WaitToGoToMainMenu());
+        }
+    }
+
+    IEnumerator WaitToGoToMainMenu()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(0);
+    }
+}
