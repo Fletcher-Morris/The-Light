b5ae7641c3c480a5daf784fc84ddd67e4fd22522
Fletcher Morris <odmoandtheboy@gmail.com>
2019-12-17 19:10:07 +0000

    Added RenderPipelines For Quallity Levels

---
+++ Assets/Scripts/Utils/Set_Render_Pipeline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.LWRP;
+
+public class Set_Render_Pipeline : MonoBehaviour
+{
+    public LightweightRenderPipelineAsset low, medium, high;
+
+    private void Start()
+    {
+        if(QualitySettings.GetQualityLevel() == 0)
+        {
+            GraphicsSettings.renderPipelineAsset = low;
+            return;
+        }
+        if (QualitySettings.GetQualityLevel() == 1)
+        {
+            GraphicsSettings.renderPipelineAsset = medium;
+            return;
+        }
+        if (QualitySettings.GetQualityLevel() >= 2)
+        {
+            GraphicsSettings.renderPipelineAsset = high;
+            return;
+        }
+    }
+}
